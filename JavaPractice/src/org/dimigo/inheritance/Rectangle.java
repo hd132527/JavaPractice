1ab7b4d5463fd83afb7f37ba7169b14a20ead0f9
Lee In Ho <m4k3r@naver.com>
2015-08-12 15:38:50 +0900

    Figure

---
+++ JavaPractice/src/org/dimigo/inheritance/Rectangle.java
@@ -0,0 +1,24 @@
+package org.dimigo.inheritance;
+
+public class Rectangle extends Figure{
+	private int height;
+	private int width;
+	
+	public Rectangle(int width, int height) {
+		super(0, 0);
+		this.width = width;
+		this.height = height;
+	}
+	
+	public Rectangle(int centerX, int centerY, int width, int height) {
+		super(centerX, centerY);
+		this.width = width;
+		this.height = height;
+	}
+
+	protected double calcArea() {
+		return (width * height);
+	}
+	
+
+}
