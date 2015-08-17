2a1046e94fe88f17cbf1b89ca5665f11b8005eec
Lee In Ho <m4k3r@naver.com>
2015-08-17 12:37:12 +0900

    Figure Upgrad2

--- JavaPractice/src/org/dimigo/inheritance/FigureTest.java
+++ JavaPractice/src/org/dimigo/inheritance/FigureTest.java
@@ -7,7 +7,21 @@ public class FigureTest {
 		Rectangle r = new Rectangle(20, 20, 5, 8);
 
 		System.out.println(String.format("원의 넓이 : %.1f", c.calcArea()));
-		System.out.println("삼각형의 넓이 : " + t.calcArea());
-		System.out.println("사각형의 넓이 : " + r.calcArea());
+		System.out.println(String.format("삼각형의 넓이 : %.1f", t.calcArea()));
+		System.out.println(String.format("사각형의 넓이 : %.1f", r.calcArea()));
+
+		c.printCenter();
+		t.printCenter();
+		r.printCenter();
+
+		System.out.println("-- 중심좌표 이동 (x,y 축 5씩)");
+		
+		c.moveFigure(5, 5);
+		t.moveFigure(5, 5);
+		r.moveFigure(5, 5);
+
+		c.printCenter();
+		t.printCenter();
+		r.printCenter();
 	}
 }
