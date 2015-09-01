b9171f9417eb1c7436111522a94824da2ba12280
Lee In Ho <m4k3r@naver.com>
2015-09-01 10:14:56 +0900

    abstractclass

---
+++ JavaPractice/src/org/dimigo/abstractclass/SmartPhoneTest.java
@@ -0,0 +1,22 @@
+package org.dimigo.abstractclass;
+
+public class SmartPhoneTest {
+	public static void main(String[] args) {
+		SmartPhone i = new IPhone("iPhone 6", "애플", 700000);
+		SmartPhone g = new Galaxy("갤럭시 S6", "삼성", 650000);
+
+		System.out.println(i);
+		i.turnOn();
+		i.pay();
+		i.useSpecialFunction();
+		i.turnOff();
+		
+		System.out.println();
+		
+		System.out.println(g);
+		g.turnOn();
+		g.pay();
+		g.useSpecialFunction();
+		g.turnOff();
+	}
+}
